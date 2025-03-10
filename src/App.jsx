d575aa2cf3e128700ba205fb25698c6d5dd0a3dc
Octuuu <125304301+Octuuu@users.noreply.github.com>
2025-03-10 14:49:00 -0400

    dia2

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,25 @@
-import { products as initialProducts } from './mocks/products.json';
-import { Products } from './components/Products.jsx';
+import { products as initialProducts } from './mocks/products.json'
+import { Products } from './components/Products.jsx'
+import { Header } from './components/Header.jsx'
+import { Footer } from './components/Footer.jsx'
+import { IS_DEVELOPMENT } from './config.js'
+import { useFilters } from './hooks/useFilters.js'
+import { Cart } from './components/Cart.jsx'
+import { CartProvider } from './context/cart.jsx'
 
-function App() {
-  const [products] = useState(initialProducts);
-  const [filters, setFilters] = useState({
-    category: 'all',
-    minPrice: 0
-  })
+function App () {
+  const { filterProducts } = useFilters()
 
-  const filterProducts = (products) => {
-    return products.filter(product => {
-      return (
-        product.price >= filters.minPrice && (
-          filters.category === 'all' || 
-          product.category === filters.category
-        )
-      )
-    })
-  }
-
-  const filteredProducts = filterProducts(products);
+  const filteredProducts = filterProducts(initialProducts)
 
   return (
-    <>
-      <Header changeFilters={setFilters} />
+    <CartProvider>
+      <Header />
+      <Cart />
       <Products products={filteredProducts} />
-    </>
-      )
+      {IS_DEVELOPMENT && <Footer />}
+    </CartProvider>
+  )
 }
 
 export default App
